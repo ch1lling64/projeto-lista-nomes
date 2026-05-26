c8bc4fda63dbc5a67e38876098c2560da1c105a0
Samuel Conceicao <samueloconceicao21@gmail.com>
2026-05-26 13:26:52 -0300

    Adicionado estrutura HTML, CSS com documentação do Bootstrap e Javascript com funções e estrutura de repetição para criar uma aplicação de lista de nomes

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let nomes = [
+    "Samuel",
+    "José",
+    "Letícia",
+    "Maria",
+    "Carlos",
+    "Pedro",
+    "Marcela",
+    "Carla",
+    "Carlos",
+]
+
+function pesquisarNome() {
+
+    let nomePesquisa = document.getElementById("campoNome").value
+    let itensLista = ""
+    for(indice in nomes) {
+        let nome = nomes[indice]
+        if(nomePesquisa == nome){
+            itensLista += `<li class="list-group-item">${nome}</li>`
+        }
+    }
+
+    document.getElementById("lista").innerHTML = itensLista
+
+}
+
+function carregarNomes() {
+
+    let itensLista = ""
+    for(indice in nomes) {
+        let nome = nomes[indice]
+        
+        itensLista += `<li class="list-group-item">${nome}</li>`
+    }
+
+    document.getElementById("lista").innerHTML = itensLista
+
+}
